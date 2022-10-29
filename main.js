e8a7d00b4eb4b2946bfe809abb91cdd3395316f9
RafaelNagiyev <114652840+RafaelNagiyev@users.noreply.github.com>
2022-10-29 16:27:00 +0400

    six done

--- main.js
+++ main.js
@@ -70,6 +70,103 @@ function five(){
     return res;
 }
 
+function six() {
+    console.log(
+        "The user enters a two-digit number. Print the literal representation of this number to the console. For example, typing '25' will display 'twenty five' in the console, and '13' will display 'thirteen'."
+      );
+      const A = parseFloat(prompt("Enter two digit number"));
+    
+      let first = "";
+      let second = "";
+      let check = false;
+      if (A / 10 >= 1) {
+        switch (parseInt((A / 10) % 10)) {
+          case 1:
+            if (A % 10 === 0) {
+              first += "ten";
+            } else if (A % 10 === 1) {
+              first += "eleven";
+            } else if (A % 10 === 2) {
+              first += "twelve";
+            } else if (A % 10 === 3) {
+              first += "thirteen";
+            } else if (A % 10 === 4) {
+              first += "fourteen";
+            } else if (A % 10 === 5) {
+              first += "fifteen";
+            } else if (A % 10 === 6) {
+              first += "sixteen";
+            } else if (A % 10 === 7) {
+              first += "seventeen";
+            } else if (A % 10 === 8) {
+              first += "eighteen";
+            } else if (A % 10 === 9) {
+              first += "nineteen";
+            }
+            check = true;
+            break;
+          case 2:
+            first += "twenty";
+            break;
+          case 3:
+            first += "thirty";
+            break;
+          case 4:
+            first += "fourty";
+            break;
+          case 5:
+            first += "fifty";
+            break;
+          case 6:
+            first += "sixty";
+            break;
+          case 7:
+            first += "seventy";
+            break;
+          case 8:
+            first += "eighty";
+            break;
+          case 9:
+            first += "ninety";
+          default:
+            break;
+        }
+    
+        if (!check) {
+          switch (A % 10) {
+            case 1:
+              second += "one";
+              break;
+            case 2:
+              second += "two";
+              break;
+            case 3:
+              second += "three";
+              break;
+            case 4:
+              second += "four";
+              break;
+            case 5:
+              second += "five";
+              break;
+            case 6:
+              second += "six";
+              break;
+            case 7:
+              second += "seven";
+              break;
+            case 8:
+              second += "eight";
+              break;
+            case 9:
+              second += "nine";
+              break;
+          }
+        }
+      }
+      console.log(first + second);
+    }
+
 function seven(){
     console.log("7. User gives 3 numbers (x, y, r). Find out if point with location (x,y) is inside a circle with radius r? (the center of the circle is at the origin)")
     let x = +prompt ('please enter the value of X:');
@@ -94,4 +191,4 @@ function seven(){
     }
     
 }
-three()
+six()
